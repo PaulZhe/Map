7b2aa043268da04220a8d8a24dccd96f7a7de0ca
涂强尧 <tuqiangyao@tuqiangyaodeMacBook-Pro.local>
2019-01-26 23:50:21 +0800

    播放语音

---
+++ Map/MAPDynamicState/Views/MAPDynamicStateTableViewCell.h
@@ -0,0 +1,33 @@
+//
+//  MAPDynamicStateTableViewCell.h
+//  Map
+//
+//  Created by 涂强尧 on 2019/1/22.
+//  Copyright © 2019 小哲的DELL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MAPDynamicStateTableViewCell : UITableViewCell <UICollectionViewDataSource,UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
+@property (nonatomic, strong) UILabel *nameLabel; //名称
+@property (nonatomic, strong) UIImageView *headImageView; //头像
+@property (nonatomic, strong) UILabel *contentLabel; //文字内容
+@property (nonatomic, strong) UILabel *replyLabel;
+@property (nonatomic, strong) UILabel *timeLabel; //时间
+@property (nonatomic, strong) UIButton *likeButton; //点赞
+@property (nonatomic, strong) UIButton *commentButton; //评论
+
+//九宫格显示图片
+@property (nonatomic, strong) UIView *picturesView;
+//播放语音
+@property (nonatomic, strong) AVAudioPlayer *audioPlayer;
+
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier typeOfMotion:(NSString *)typeString;
+
+@end
+
+NS_ASSUME_NONNULL_END
