61cac113ea490e274a192a08e51d341afa68f22b
涂强尧 <tuqiangyao@tuqiangyaodeMacBook-Pro.local>
2019-01-27 00:03:11 +0800

    播放录音

---
+++ Map/MAPDynamicState/Views/DynamicStateView/MAPDynamicStateTableViewCell.h
@@ -0,0 +1,33 @@
+//
+//  MAPDynamicStateTableViewCell.h
+//  Map
+//
+//  Created by 涂强尧 on 2019/1/26.
+//  Copyright © 2019 小哲的DELL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MAPMotivePicturesView.h"
+#import "MAPMotiveAudioButton.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MAPDynamicStateTableViewCell : UITableViewCell
+@property (nonatomic, strong) UILabel *nameLabel;
+@property (nonatomic, strong) UIImageView *headImageView;
+@property (nonatomic, strong) UILabel *contentLabel;
+@property (nonatomic, strong) UILabel *replyLabel;
+@property (nonatomic, strong) UILabel *timeLabel;
+@property (nonatomic, strong) UIButton *likeButton;
+@property (nonatomic, strong) UIButton *commentButton;
+
+
+@property (nonatomic, strong) MAPMotivePicturesView *picturesView;//九宫格显示图片
+@property (nonatomic, strong) MAPMotiveAudioButton *audioButton;//播放语音
+
+//重写init方法，实现cell的封装
+- (instancetype) initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier typeOfMotion:(nonnull NSString *)typeString;
+
+@end
+
+NS_ASSUME_NONNULL_END
