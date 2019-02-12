09be9fd3149f004b03750f80a54199ba5903c2d6
tqy582144930 <582144930@qq.com>
2019-02-12 16:52:04 +0800

    添加manager

---
+++ Map/MAPAddDynamicState/Models/MAPAddDynamicStateJSONModel.h
@@ -0,0 +1,17 @@
+//
+//  MAPAddDynamicStateJSONModel.h
+//  Map
+//
+//  Created by 涂强尧 on 2019/2/12.
+//  Copyright © 2019 小哲的DELL. All rights reserved.
+//
+
+#import "JSONModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MAPAddDynamicStateJSONModel : JSONModel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Map/MAPAddDynamicState/Models/MAPAddDynamicStateJSONModel.m
@@ -0,0 +1,13 @@
+//
+//  MAPAddDynamicStateJSONModel.m
+//  Map
+//
+//  Created by 涂强尧 on 2019/2/12.
+//  Copyright © 2019 小哲的DELL. All rights reserved.
+//
+
+#import "MAPAddDynamicStateJSONModel.h"
+
+@implementation MAPAddDynamicStateJSONModel
+
+@end
