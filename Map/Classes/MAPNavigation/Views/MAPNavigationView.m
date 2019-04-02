db246da3c4c2366bb868f1cb104dcfe4ee9ce5a7
tqy582144930 <582144930@qq.com>
2019-04-02 20:02:59 +0800

    导航

--- Map/Classes/MAPNavigation/Views/MAPNavigationView.m
+++ Map/Classes/MAPNavigation/Views/MAPNavigationView.m
@@ -22,10 +22,11 @@
         _navigationView.backgroundColor = [UIColor whiteColor];
         [self addSubview:_navigationView];
         
-//        _loactionTableView = [[UITableView alloc] init];
-//        _loactionTableView.dataSource = self;
-//        _loactionTableView.delegate = self;
-//        [_navigationView addSubview:_loactionTableView];
+        _loactionTableView = [[UITableView alloc] init];
+        _loactionTableView.dataSource = self;
+        _loactionTableView.delegate = self;
+        [_loactionTableView registerClass:[MAPLocationTableViewCell class] forCellReuseIdentifier:@"location"];
+        [_navigationView addSubview:_loactionTableView];
         
         _checkButton = [[UIButton alloc] init];
         [_checkButton setTitle:[NSString stringWithFormat:@"查看路线"] forState:UIControlStateNormal];
@@ -53,12 +54,12 @@
         make.height.mas_equalTo(self.mas_height).multipliedBy(0.65);
     }];
     
-//    [_loactionTableView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.top.mas_equalTo(self.navigationView.mas_top).mas_offset(20);
-//        make.left.mas_equalTo(self.navigationView.mas_left).mas_offset(10);
-//        make.right.mas_equalTo(self.navigationView.mas_right).mas_offset(-10);
-//        make.bottom.mas_equalTo(self.navigationView.mas_bottom).mas_offset(-60);
-//    }];
+    [_loactionTableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.navigationView.mas_top).mas_offset(20);
+        make.left.mas_equalTo(self.navigationView.mas_left).mas_offset(10);
+        make.right.mas_equalTo(self.navigationView.mas_right).mas_offset(-10);
+        make.bottom.mas_equalTo(self.navigationView.mas_bottom).mas_offset(-60);
+    }];
     
     [_checkButton mas_makeConstraints:^(MASConstraintMaker *make) {
         make.height.mas_equalTo(60);
@@ -66,6 +67,23 @@
         make.right.mas_equalTo(self.navigationView.mas_right).mas_offset(0);
         make.bottom.mas_equalTo(self.navigationView.mas_bottom).mas_offset(0);
     }];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    MAPLocationTableViewCell *cell = [_loactionTableView dequeueReusableCellWithIdentifier:@"location" forIndexPath:indexPath];
+    if (cell) {
+        NSArray *array = [NSArray arrayWithObjects:@"起点", @"终点", @"1", nil];
+        cell.locationTextField.text = array[indexPath.row];
+    }
+    return cell;
 }
 
 //查看路线点击事件
